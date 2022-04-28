57f8408a155d5cd779414908aa985bf397d8be39
Robson <csrbson@gmail.com>
2022-04-28 00:44:56 -0300

    funções implementadas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,27 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== '') {     
+      const task: Task = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        isComplete: false
+      }      
+      setTasks(tasks => [...tasks, task])
+    } else {
+      alert('tarefa deve ter um titulo')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.filter(task => task.id === id)
+    setTasks(tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : task))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
